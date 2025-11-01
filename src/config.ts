2dcfa783948d59f275fa7b39d211dc9c4523c231
Eric Rizzi <erizzi@amazon.com>
2025-11-01 13:54:26 -0500

    Bugfix: Unnecessary trailing slash in config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const BASE_PATH = import.meta.env.BASE_URL;
 
 export const API_GATEWAY_BASE_URL =
-  "https://txg564rkl8.execute-api.us-west-1.amazonaws.com/";
+  "https://txg564rkl8.execute-api.us-west-1.amazonaws.com";
 
 export const GOOGLE_CLIENT_ID =
   "111269738814-64jdhnq0krkbgdk7f1lubtf7oc1fkr8n.apps.googleusercontent.com";
